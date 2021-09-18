9a532e17eb725e5d4e3b43ef266c1e16388c9e1d
skconan <supakit.kr@gmail.com>
2021-09-18 10:02:37 +0700

    Add: hist of gray

--- image_processing/09_histogram.py
+++ image_processing/09_histogram.py
@@ -4,12 +4,18 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('images/test_img.png')
 
-histogram = cv2.calcHist([img], [0], None, [256], [0, 256])
-plt.hist(img.ravel(), 256, [0, 256])
-plt.show()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('img', gray)
+histogram = cv2.calcHist([gray], [0], None, [256], [0, 256])
+
+plt.title("Histogram of gray image")
+plt.plot(histogram)
+plt.figure()
 
 color = ['b', 'g', 'r']
+plt.title("Histogram of BGR image")
 for i in range(len(color)):
     histogram2 = cv2.calcHist([img], [i], None, [256], [0, 256])
     plt.plot(histogram2, color=color[i])
 plt.show()
+cv2.waitKey(-1)
